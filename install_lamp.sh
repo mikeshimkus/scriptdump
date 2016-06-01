51ca58bb4ece75f8e4c783fa8fe6accf85c1bc51
edwoodjrjr <edwoodjrjr@users.noreply.github.com>
2016-06-01 08:36:55 -0700

    changed command

--- install_lamp.sh
+++ install_lamp.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 sudo apt-get -y update
 
-# install the LAMP stack
-sudo apt-get -y install apache2 php5 libapache2-mod-php5 php5-mcrypt php5-curl php5-mysql php5-gd php5-cli php5-dev mysql-client
-php5enmod mcrypt
-
 # set up a silent install of MySQL
 # the following commands set the MySQL root password when you install the mysql-server package.
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password Pass1word'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password Pass1word'
 
-sudo apt-get -y install mysql-server
+# install the LAMP stack
+sudo apt-get -y install lamp-server^
 
 #Restart all the installed services to verify that everything is installed properly
 
