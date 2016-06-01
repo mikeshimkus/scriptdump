2ecfb51a1435bfcab107a1a81c9f91d3d4a6697a
edwoodjrjr <edwoodjrjr@users.noreply.github.com>
2016-06-01 12:37:28 -0700

    go to sleep

--- install_lamp.sh
+++ install_lamp.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 sleep 30
-apt-get update
+apt-get update && apt-get upgrade
+
+apt-get -y install apache2
 
 echo mysql-server mysql-server/root_password password $1 | debconf-set-selections  
 echo mysql-server mysql-server/root_password_again password $1 | debconf-set-selections 
 
 apt-get -y install mysql-server
 
-apt-get -y install lamp-server^
+apt-get -y install php5 libapache2-mod-php5
+
+/etc/init.d/apache2 restart
