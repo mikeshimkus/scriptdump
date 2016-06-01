adb8d3cf0b668804f75b5eeacba88f43fc3d8dc2
edwoodjrjr <edwoodjrjr@users.noreply.github.com>
2016-06-01 11:33:03 -0700

    awesome

--- install_lamp.sh
+++ install_lamp.sh
@@ -4,4 +4,6 @@ apt-get update
 echo mysql-server mysql-server/root_password password $1 | debconf-set-selections  
 echo mysql-server mysql-server/root_password_again password $1 | debconf-set-selections 
 
+apt-get -y install mysql-server
+
 apt-get -y install lamp-server^
