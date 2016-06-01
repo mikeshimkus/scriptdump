36fce54791a1d229f7ee9c591eb4bb9a5e2d2b21
edwoodjrjr <edwoodjrjr@users.noreply.github.com>
2016-06-01 10:32:02 -0700

    mo updates

--- install_lamp.sh
+++ install_lamp.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 apt-get update
 
-# set up a silent install of MySQL
-# the following commands set the MySQL root password when you install the mysql-server package.
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password Pass1word'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password Pass1word'
+apt-get -y install build-essential libssl-dev binutils binutils-dev openssl
+apt-get -y install libdb-dev libexpat1-dev automake checkinstall unzip elinks sshpass
 
-# install the LAMP stack
-apt-get -y install lamp-server^
+apt-get -y install apache2 libapache2-mod-perl2
+apt-get -y install libcrypt-ssleay-perl libwww-perl libhtml-parser-perl libwww-mechanize-perl
+apt-get -y install php5
+a2enmod ssl
+a2ensite default-ssl
+apt-get -y install php5-mcrypt
+php5enmod mcrypt
+apt-get -y install php5-gd
+apt-get -y install php5-curl
+apt-get -y install php5-mysql
+service apache2 restart
 
-#Restart all the installed services to verify that everything is installed properly
+apt-get -y install build-essential libssl-dev binutils binutils-dev openssl
+apt-get -y install libdb-dev libexpat1-dev automake checkinstall unzip elinks
 
-echo -e "\n"
+echo mysql-server mysql-server/root_password password $1 | debconf-set-selections  
+echo mysql-server mysql-server/root_password_again password $1 | debconf-set-selections 
 
-service apache2 restart && service mysql restart > /dev/null
-
-echo -e "\n"
-
-if [ $? -ne 0 ]; then
-   echo "Please Check the Install Services, There is some $(tput bold)$(tput setaf 1)Problem$(tput sgr0)
-else
-   echo "Installed Services run $(tput bold)$(tput setaf 2)Sucessfully$(tput sgr0)"
-fi
-
-echo -e "\n"
+apt-get -y install mysql-server
+apt-get -y install libmysqld-dev libdb-dev
