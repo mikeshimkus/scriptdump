9f52c13898c74799c3bff2dbbcc612e53565f9c0
Mike Shimkus <edwoodjrjr@users.noreply.github.com>
2016-10-27 11:49:02 -0700

    more stuff

--- main.sh
+++ main.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+while getopts s:k: option
+do	case "$option"  in
+        s) storageacct=$OPTARG;;
+        k) key=$OPTARG;;
+    esac 
+done
+
 mkdir -m 777 /var/tmp/mystuff
-touch /var/tmp/mystuff/mystuff.json
+touch /var/tmp/mystuff/mystuff.txt
+
+echo $storageacct >> /var/tmp/mystuff/mystuff.txt
+echo $key >> /var/tmp/mystuff/mystuff.txt
+exit
