a705f8259349d8c9ea23a3693a5d9964d785025d
Mike Shimkus <edwoodjrjr@users.noreply.github.com>
2016-11-29 10:12:29 -0800

    Update main.sh
    echo new variables

--- main.sh
+++ main.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
-while getopts s:k: option
+while getopts s:k:i:t:c:p: option
 do	case "$option"  in
-        s) storageacct=$OPTARG;;
-        k) key=$OPTARG;;
+        s) storage_acct=$OPTARG;;
+        k) storage_key=$OPTARG;;
+        i) subscription_id=$OPTARG;;
+        t) tenant_id=$OPTARG;;
+        c) client_id=$OPTARG;;
+        p) sp_secret=$OPTARG;;
     esac 
 done
 
 mkdir -m 777 /var/tmp/mystuff
 touch /var/tmp/mystuff/mystuff.txt
 
-echo $storageacct >> /var/tmp/mystuff/mystuff.txt
-echo $key >> /var/tmp/mystuff/mystuff.txt
+echo $storage_acct >> /var/tmp/mystuff/mystuff.txt
+echo $storage_key >> /var/tmp/mystuff/mystuff.txt
+echo $subscription_id >> /var/tmp/mystuff/mystuff.txt
+echo $tenant_id >> /var/tmp/mystuff/mystuff.txt
+echo $client_id >> /var/tmp/mystuff/mystuff.txt
+echo $sp_secret >> /var/tmp/mystuff/mystuff.txt
 exit
