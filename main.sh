9c6da287aecb7c18edd3dee47a582012840eb32a
Mike Shimkus <edwoodjrjr@users.noreply.github.com>
2016-11-29 14:32:36 -0800

    added resource group

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-while getopts s:k:i:t:c:p:v: option
+while getopts s:k:i:t:c:p:r:v: option
 do	case "$option"  in
         s) storage_acct=$OPTARG;;
         k) storage_key=$OPTARG;;
@@ -7,6 +7,7 @@ do	case "$option"  in
         t) tenant_id=$OPTARG;;
         c) client_id=$OPTARG;;
         p) sp_secret=$OPTARG;;
+        r) resource_group=$OPTARG;;
         v) vmss_name=$OPTARG;;
     esac 
 done
@@ -14,6 +15,6 @@ done
 mkdir -m 777 /var/tmp/azure_dict
 touch /var/tmp/azure_dict/az.json
 
-json="{"\"storageAccount\"":"\"$storage_acct\"","\"storageKey\"":"\"$storage_key\"","\"subscriptionId\"":"\"$subscription_id\"","\"tenantId\"":"\"$tenant_id\"","\"clientId\"":"\"$client_id\"","\"secret\"":"\"$sp_secret\"","\"vmss\"":"\"$vmss_name\""}"
+json="{"\"storageAccount\"":"\"$storage_acct\"","\"storageKey\"":"\"$storage_key\"","\"subscriptionId\"":"\"$subscription_id\"","\"tenantId\"":"\"$tenant_id\"","\"clientId\"":"\"$client_id\"","\"secret\"":"\"$sp_secret\"","\"scaleSet\"":"\"$vmss_name\"","\"resourceGroup\"":"\"$resource_group\""}"
 echo $json >> /var/tmp/azure_dict/az.json
 exit
