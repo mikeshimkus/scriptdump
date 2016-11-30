64d7fea3987cf1d32c1d9489dd2b3cd74d28f473
Mike Shimkus <edwoodjrjr@users.noreply.github.com>
2016-11-30 11:40:51 -0800

    onboarding

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-while getopts s:k:i:t:c:p:r:v: option
-do	case "$option"  in
+while getopts s:k:i:t:c:p:r:v:u:x: option
+do      case "$option"  in
         s) storage_acct=$OPTARG;;
         k) storage_key=$OPTARG;;
         i) subscription_id=$OPTARG;;
@@ -9,12 +9,18 @@ do	case "$option"  in
         p) sp_secret=$OPTARG;;
         r) resource_group=$OPTARG;;
         v) vmss_name=$OPTARG;;
-    esac 
+        u) user=$OPTARG;;
+        x) passwd=$OPTARG;;
+    esac
 done
 
-mkdir -m 777 /var/tmp/azure_dict
-touch /var/tmp/azure_dict/az.json
+selfip=$(tmsh list net self self_1nic address | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}')
+echo "Self IP is: $selfip"
+lastoctet=`echo $selfip | cut -d . -f 4`
+echo "Last octet: $lastoctet"
+instance=`expr $lastoctet - 4`
+echo "Instance: $instance"
 
-json="{"\"storageAccount\"":"\"$storage_acct\"","\"storageKey\"":"\"$storage_key\"","\"subscriptionId\"":"\"$subscription_id\"","\"tenantId\"":"\"$tenant_id\"","\"clientId\"":"\"$client_id\"","\"secret\"":"\"$sp_secret\"","\"scaleSet\"":"\"$vmss_name\"","\"resourceGroup\"":"\"$resource_group\""}"
-echo $json >> /var/tmp/azure_dict/az.json
+exec f5-rest-node /var/lib/waagent/custom-script/download/0/runScripts.js --log-level debug --tag v1.2.0 --onboard "--output /var/log/onboard.log --log-level debug --host $selfip -u admin -p $passwd --hostname $vmss_name$instance.azuresecurity.com --set-password admin:$passwd --db provision.1nicautoconfig:disable --db tmm.maxremoteloglength:2048 --module ltm:nominal --module asm:nominal --module afm:none --update-sigs"
+
 exit
